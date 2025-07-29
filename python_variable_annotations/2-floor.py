af5696a073451bdfd409b18627fd2deb08f2e57c
Leen2025 <leeeen4@hotmail.com>
2025-07-29 20:21:54 +0000

    2-floor.py

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -2,7 +2,8 @@
 '''Task 2's module.
 '''
 
-def floor(a:float) -> int:
-     '''Computes the floor of a floating-point number.
+
+def floor(a: float) -> int:
+    '''Computes the floor of a floating-point number.
     '''
     return int(a)
