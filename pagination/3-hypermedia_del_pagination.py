6de500243491209aeba983915678310911353f3a
Leen2025 <leeeen4@hotmail.com>
2025-08-12 11:56:49 +0000

    update

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""Deletion-resilient hypermedia pagination
-"""
+"""Deletion-resilient hypermedia pagination"""
 
 import csv
 import math
@@ -8,18 +7,12 @@ from typing import List, Dict, Tuple
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
         self.__indexed_dataset = None
 
-
-        return {
-            "index": index,
-            "next_index": i,
-            "page_size": page_size,
-            "data": d
-        }
+        return {"index": index, "next_index": i, "page_size": page_size, "data": d}
