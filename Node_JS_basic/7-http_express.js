91b7b4623660d9c38882d6822bdfd334dc584fd6
Leen2025 <leeeen4@hotmail.com>
2025-09-05 22:14:39 +0000

    7-http_express.js

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,64 +1,31 @@
-// 7-http_express.js
 const express = require('express');
-const fs = require('fs');
-const process = require('process');
+const countStudents = require('./3-read_file_async');
 
 const app = express();
-const port = 1245;
-
-// Function to read CSV and return formatted student info
-function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
-
-      const lines = data.split('\n').filter(line => line.trim() !== '');
-      const students = lines.slice(1).map(line => {
-        const [firstname, lastname, age, field] = line.split(',');
-        return { firstname, lastname, age, field };
-      });
-
-      const fields = {};
-      students.forEach(student => {
-        if (!fields[student.field]) fields[student.field] = [];
-        fields[student.field].push(student.firstname);
-      });
-
-      let output = `Number of students: ${students.length}\n`;
-      for (const [field, names] of Object.entries(fields)) {
-        output += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
-      }
-      resolve(output.trim());
-    });
-  });
-}
-
-// Root route
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-// Students route
 app.get('/students', async (req, res) => {
-  const dbFile = process.argv[2];
-  if (!dbFile) {
-    res.status(500).send('Database file not provided');
+  const dbpath = process.argv[2];
+  let o = 'This is the list of our students\n';
+  if (!dbpath) {
+    o += 'Cannot load the database';
+    res.status(500).send(o);
     return;
   }
-
   try {
-    const studentInfo = await countStudents(dbFile);
-    res.send(`This is the list of our students\n${studentInfo}`);
-  } catch (err) {
-    res.status(500).send(err.message);
+    const studentsData = await countStudents(dbpath);
+    o += studentsData;
+    res.send(o);
+  } catch (error) {
+    o += 'Cannot load the database';
+    res.send(o);
   }
 });
 
-app.listen(port, () => {
-  // Server is running
+app.listen(1245, () => {
+  console.log('Express server is listening on port 1245');
 });
 
 module.exports = app;
